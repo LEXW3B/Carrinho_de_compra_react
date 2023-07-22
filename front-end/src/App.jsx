4e48daf0de9c92733e44b005422101bbb8b93ec7
Ale <alexmanson12345@gmail.com>
2023-07-22 13:16:25 -0300

    feat: adding products to cart

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import './styles.scss';
 import TableRow from './TableRow';
@@ -6,8 +6,52 @@ import PageHeader from './layout/PageHeader';
 import PageTitle from './layout/PageTitle';
 import Summary from './Summary';
 
+import {api} from './provider';
+
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
 function App() {
   const [cart, setCart] = useState([]);
+
+  const productObject = {
+    name: 'produto',
+    category: 'categoria',
+    price: randomNumber(90, 1200),
+    quantity: 1,
+  }
+
+  const fetchData = () => {
+    api.get('/cart').then((response) => setCart(response.data));
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleAddItem = () => {
+    // inserção
+    console.log('Disparou  handleAddItem');
+
+    api.post('/cart', productObject).then(response => {
+      console.log(response);
+
+      fetchData();
+    });
+  }
+
+  const handleRemoveItem = () => {
+    // remoção
+
+
+  }
+
+  const handleUpdateItem = () => {
+    // atualização de quantidade
+
+
+  }
 
   return (
     <>
@@ -16,6 +60,12 @@ function App() {
         <PageTitle data={'Seu carrinho'} />
         <div className="content">
           <section>
+            <button
+              onClick={handleAddItem}
+              style={{ padding: '5px 10xp', marginBottom: '15px', background: 'red' }}
+            >
+              add to cart
+            </button>
             <table>
               <thead>
                 <tr>
--- front-end/src/provider.js
+++ front-end/src/provider.js
@@ -3,5 +3,5 @@ import axios from 'axios';
 export const api = axios.create({
   baseURL: 'https://crudcrud.com/api/fbd2ec4ccf9b4e599354d5d4f37aee6b/', /*insira a url do cudcrud*/
   timeout: 1000,
-  headers: {'X-Custom-Header': 'foobar'}
+  // headers: {'X-Custom-Header': 'foobar'} esta parte da erro
 });
