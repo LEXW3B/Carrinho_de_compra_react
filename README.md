d5fabdf3344fe87e5c301bce14bb74be99b335e5
Ale <alexmanson12345@gmail.com>
2023-07-22 10:08:18 -0300

    feat: Made a placeholder for when the shopping cart is empty

--- README.md
+++
@@ -1,2 +0,0 @@
-# Carrinho_de_compra_react
-Um carrinho de compras funcional feito em react, com inserção e remoção, alteração de quantidade, cálculo de preço e desconto de produto.
--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './styles.scss';
 import TableRow from './TableRow';
@@ -7,10 +7,11 @@ import PageTitle from './layout/PageTitle';
 import Summary from './Summary';
 
 function App() {
+  const [cart, setCart] = useState([]);
+
   return (
     <>
       <PageHeader />
-
       <main>
         <PageTitle data={'Seu carrinho'} />
         <div className="content">
@@ -27,7 +28,14 @@ function App() {
               </thead>
 
               <tbody>
-                <TableRow />
+                {cart.map((item) => <TableRow />)}
+                {cart.length === 0 && (
+                  <tr>
+                    <td colSpan={'5'} style={{textAlign: 'center'}}>
+                      <b>Carrinho de compras vazio.</b>
+                    </td>
+                  </tr>
+                )}              
               </tbody>
             </table>
           </section>
